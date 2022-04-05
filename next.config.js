caaaa96ee66f6e4e12be3610265398c071874ecd
Adebayo Ajayi <9973353-Braggedtooth@users.noreply.gitlab.com>
2022-04-05 03:12:03 +0200

    fix build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 const withImages = require('next-images')
-module.exports = withImages()
+
 module.exports = {
+
   reactStrictMode: true,
-  env: {
-    API_URL: 'http://localhost:3103'
+  withImages,
+  async redirects () {
+    return [
+      {
+        source: '/register',
+        destination: '/login',
+        permanent: true
+      }
+    ]
   }
 }
