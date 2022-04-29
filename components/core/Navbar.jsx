5c153e74f96650f1e25b824aa094eec2adad5809
Adebayo Ajayi <54339202+Braggedtooth@users.noreply.github.com>
2022-04-30 00:05:55 +0200

    setup admin page

--- components/core/Navbar.jsx
+++ components/core/Navbar.jsx
@@ -1,7 +1,14 @@
 import { Anchor, Burger, Group, Header, Menu, MenuItem, Text, useMantineTheme } from '@mantine/core'
 import Link from 'next/link'
 import React, { useState } from 'react'
-import { MdAttribution, MdLogin, MdLogout, MdPerson, MdReviews } from 'react-icons/md'
+import {
+  MdAttribution,
+  MdLogin,
+  MdLogout,
+  MdPerson,
+  MdReviews,
+  MdAdminPanelSettings
+} from 'react-icons/md'
 import useProfile from '../../services/hooks/useProfile'
 import useStore from '../../services/hooks/useStore'
 import Logo1 from './Logo'
@@ -9,51 +16,60 @@ import Logo1 from './Logo'
 const MenuItems = () => {
   const { LogOut } = useProfile()
   const [opened, setOpened] = useState(false)
-  const { isLoggedIn } = useStore().store
+  const { isLoggedIn, user } = useStore().store
   const theme = useMantineTheme()
   return (
     <Menu
-      placement='center'
+      placement="center"
       gutter={6}
       onClose={() => setOpened(false)}
       control={
         <Burger
           opened={opened}
           onClick={() => setOpened(!opened)}
-          size='md'
+          size="md"
           color={theme.colors.gray[3]}
         />
       }
     >
-    {isLoggedIn &&    
-    <MenuItem icon={<MdPerson />} component={Anchor} href='/mina-sidor'>
-        <Text size='lg'>
-          Mina Sidor
-        </Text>
-      </MenuItem>}
-      <MenuItem icon={<MdAttribution/>} component={Anchor} href='/om-oss'>
-        <Text size='lg'>
-          Om oss
-        </Text>
+      {isLoggedIn && (
+        <MenuItem icon={<MdPerson />} component={Anchor} href="/mina-sidor">
+          <Text size="lg">Mina Sidor</Text>
+        </MenuItem>
+      )}
+      <MenuItem icon={<MdAttribution />} component={Anchor} href="/om-oss">
+        <Text size="lg">Om oss</Text>
       </MenuItem>
-      <MenuItem icon={<MdReviews />} component={Anchor} href='/recensioner'>
-        <Text size='lg'>
-          Recensioner
-        </Text>
+      <MenuItem icon={<MdReviews />} component={Anchor} href="/recensioner">
+        <Text size="lg">Recensioner</Text>
       </MenuItem>
-     {!isLoggedIn ?  <MenuItem component={Anchor} href='/logga-in' icon={<MdLogin/>}>
-        <Text size='lg' color='green'>
-          Logga in
-        </Text>
-      </MenuItem> : ( <MenuItem
-        icon={<MdLogout />}
-        color='red'
-        onClick={async () => {
-          await LogOut(undefined, undefined)
-        }}
-      > <Text size='lg'>
-        Logga ut
-        </Text> </MenuItem> ) }
+      {user.role === 'ADMIN' && (
+        <MenuItem
+          icon={<MdAdminPanelSettings />}
+          component={Anchor}
+          href="/admin"
+        >
+          <Text size="lg">Admin</Text>
+        </MenuItem>
+      )}
+      {!isLoggedIn ? (
+        <MenuItem component={Anchor} href="/logga-in" icon={<MdLogin />}>
+          <Text size="lg" color="green">
+            Logga in
+          </Text>
+        </MenuItem>
+      ) : (
+        <MenuItem
+          icon={<MdLogout />}
+          color="red"
+          onClick={async () => {
+            await LogOut(undefined, undefined)
+          }}
+        >
+          {' '}
+          <Text size="lg">Logga ut</Text>{' '}
+        </MenuItem>
+      )}
     </Menu>
   )
 }
